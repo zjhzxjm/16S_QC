8c85b51e688a00b0fc2c2ffd09b5219b10afdc55
zjhzxjm <416279164@qq.com>
2016-06-12 13:35:47 +0800

    fix for dev env

--- workflow/config.py
+++ workflow/config.py
@@ -1,3 +1,3 @@
 ENV_BIN = "/data_center_01/home/xujm/soft/Anaconda/anaconda3/envs/snakemake/bin/"
-SCRIPTS_BIN = "/data_center_01/home/xujm/Pipeline/16S_QC/rules/scripts/"
+SCRIPTS_BIN = "/data_center_01/home/xujm/bin/.self/Pipeline_dev/16S_QC/rules/scripts/"
 PANDASEQ = "/data_center_01/home/NEOLINE/liangzebin/soft/pandaseq_rebuild/bin/pandaseq "
